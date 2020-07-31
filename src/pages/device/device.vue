086f86616bb674eb2c5ecadc03ccb2f56fb5ab39
weijiqiang <weijq@cychina.cn>
2020-07-31 17:44:15 +0800

    设备详情页面开发
    Signed-off-by: weijiqiang <weijq@cychina.cn>

--- src/pages/device/device.vue
+++ src/pages/device/device.vue
@@ -1,10 +1,12 @@
 <template>
-  <view class="device-page">
+  <view class="device-page" :style="{background: `${swiperStyle[current][3]}`}">
     <img src="../../static/img/back.png" alt @click="backToPre" class="back-to-pre" />
     <view class="device-content">
       <view class="dvice-info">
-        <view class="device-type-name">雾灯</view>
-        <view class="device-dvice-name">{{siteInfo['devices'][0]['device_name']}}</view>
+        <view
+          class="device-type-name"
+        >{{deviceTypeDic[siteInfo['devices'][current]['device_type']]}}</view>
+        <view class="device-dvice-name">{{siteInfo['devices'][current]['device_name']}}</view>
         <view class="device-site-name">{{siteInfo['sysName']}}</view>
       </view>
       <view class="info-detail">
@@ -16,19 +18,42 @@
             :interval="interval"
             :duration="duration"
             circular="{true}"
-            previous-margin="4px"
-            next-margin="4px"
+            :current="current"
+            v-model="current"
+            @change="currentChange"
           >
-            <swiper-item class="swiper-item">
-              <view class="swiper-item uni-bg-area">
-                <view class="item-content">AAA</view>
+            <swiper-item
+              :class="['swiper-item-container', current == 0?'currentPanel':'otherPanel']"
+              :style="{position: 'absolute',width: '100%',
+height: '100%',transform: `translate(${swiperStyle[current][0]}%, 0px) translateZ(0px)`}"
+            >
+              <view class="swiper-item uni-bg-area A">
+                <view
+                  class="item-content"
+                >{{deviceTypeDic[siteInfo['devices'][current]['device_type']]}}</view>
               </view>
             </swiper-item>
-            <swiper-item class="swiper-item">
-              <view class="swiper-item uni-bg-area"><view class="item-content">BBB</view></view>
+            <swiper-item
+              :class="['swiper-item-container', current == 1?'currentPanel':'otherPanel']"
+              :style="{position: 'absolute',width: '100%',
+height: '100%',transform: `translate(${swiperStyle[current][1]}%, 0px) translateZ(0px)`}"
+            >
+              <view class="swiper-item uni-bg-area B">
+                <view
+                  class="item-content"
+                >{{deviceTypeDic[siteInfo['devices'][current]['device_type']]}}</view>
+              </view>
             </swiper-item>
-            <swiper-item class="swiper-item">
-              <view class="swiper-item uni-bg-area"><view class="item-content">CCC</view></view>
+            <swiper-item
+              :class="['swiper-item-container', current == 2?'currentPanel':'otherPanel']"
+              :style="{position: 'absolute',width: '100%',
+height: '100%',transform: `translate(${swiperStyle[current][2]}%, 0px) translateZ(0px)`}"
+            >
+              <view class="swiper-item uni-bg-area C">
+                <view
+                  class="item-content"
+                >{{deviceTypeDic[siteInfo['devices'][current]['device_type']]}}</view>
+              </view>
             </swiper-item>
           </swiper>
         </view>
@@ -48,6 +73,19 @@ export default {
     return {
       siteInfo: this.siteInfo,
       deviceSelect: 3,
+      current: 0,
+      newCurrent: 0,
+      swiperStyle: {
+        //0:[a,b,c]表示对应状态下 panel对应的X偏移
+        0: [0, 100, -100, "#74c69d"],
+        1: [28, 100, 229, "#48bfe3"],
+        2: [357, 156, 200, "#adb5bd"],
+      },
+      deviceTypeDic: {
+        3: "防撞雾灯",
+        6: "能见度仪",
+        7: "视频",
+      },
     };
   },
   methods: {
@@ -55,6 +93,16 @@ export default {
       uni.navigateBack({
         delta: 1,
       });
+    },
+    currentChange(e) {
+      console.log(e, 88888);
+      this.current = e.detail.current;
+    },
+  },
+  watch: {
+    current: function (oldVal, newVal) {
+      debugger;
+      console.log(oldVal, newVal, 9999);
     },
   },
 };
@@ -85,9 +133,10 @@ export default {
 
 .device-dvice-name,
 .device-site-name {
-  font-size: 12px;
+  font-size: 13px;
   padding: 2px 10px;
-  color: #ccc;
+  color: #f0f0f0;
+  /* font-weight: 700; */
 }
 
 .device-type-name {
@@ -113,16 +162,48 @@ export default {
 
 .swiper {
   height: calc(100vh - 210px);
+  padding-top: 10px;
 }
 
 .swiper-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 10px;
-  width: 90%;
+  width: 85%;
   border-radius: 10px;
   /* box-shadow: 0 0px 1px 1px rgba(255, 255, 255, 0.1); */
 }
 
-.item-content{
-  padding:10px;
+.item-content {
+  padding: 10px;
+}
+
+.swiper-item-container {
+  display: flex;
+  justify-content: center;
+}
+
+.A {
+  color: #fff;
+  background: #52b788;
+}
+
+.B {
+  color: #fff;
+  background: #4ea8de;
+}
+
+.C {
+  color: #fff;
+  background: #6c757d;
+}
+
+.currentPanel {
+  scale: 1;
+}
+
+.otherPanel {
+  scale: 0.78;
 }
 </style>
